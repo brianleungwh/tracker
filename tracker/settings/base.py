9f08c1a3a4878c69e7117c12473873daf7976b9e
brianleung <bleungwh@gmail.com>
2016-08-26 22:02:40 -0700

    create env directory. modify docker-compose.yml. start api app.

--- tracker/settings/base.py
+++ tracker/settings/base.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'tracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
