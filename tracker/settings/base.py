3a187f5f3b12912bf787e590215fc2af8cfe6589
brianleung <bleungwh@gmail.com>
2016-10-01 10:04:21 -0700

    Make index page

--- tracker/settings/base.py
+++ tracker/settings/base.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
