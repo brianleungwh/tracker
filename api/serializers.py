b8c45850b46102332281c997ee25b724dd08e6f0
brianleung <bleungwh@gmail.com>
2016-10-02 09:07:32 -0700

    Fix typo

--- api/serializers.py
+++ api/serializers.py
@@ -69,7 +69,7 @@ class UserTrackerSerializer(serializers.Serializer):
 
     def delete_tracker(self):
         if self.tracker_does_not_already_exist():
-            raise serializers.ValidationError('Cannot delete Track that does not exist')
+            raise serializers.ValidationError('Cannot delete Tracker that does not exist')
         email, results_page_url = self.extract_validated_data()
         user = User.objects.get(email=email)
         user.tracker_set.get(results_page_url=results_page_url).delete()
