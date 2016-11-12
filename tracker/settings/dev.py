710b69beb30806dcb76805533da8ed9de1a999f8
brianleung <bleungwh@gmail.com>
2016-11-12 15:04:54 -0800

    Make celery beat run every 4 hours in production

--- tracker/settings/dev.py
+++ tracker/settings/dev.py
@@ -1,1 +1,9 @@
 from base import *
+
+# schedule celery beat task to run every minute for easy testing
+CELERYBEAT_SCHEDULE = {
+    'update-trackers': {
+        'task': 'api.update_trackers',
+        'schedule': crontab(minute='*/1'),
+    },
+}
--- tracker/settings/prod.py
+++ tracker/settings/prod.py
@@ -4,3 +4,10 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 # SESSION_COOKIE_HTTPONLY = False
 # CSRF_COOKIE_HTTPONLY = False
+
+CELERYBEAT_SCHEDULE = {
+    'update-trackers': {
+        'task': 'api.update_trackers',
+        'schedule': crontab(minute=0, hour='*/4'),
+    },
+}
